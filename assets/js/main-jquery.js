5d2b2d6f152125e855e1356f034651ce9a8e05d7
Kev <kevleitch@gmail.com>
2021-03-08 20:12:10 +0000

    New styling
    New styling throughout and random recipe totals

--- assets/js/main-jquery.js
+++ assets/js/main-jquery.js
@@ -1,12 +1,4 @@
 $(document).ready(function(){
-	$('.show-search-form').click(function(e) {
-		$('#search-form').removeClass('hide-search').addClass('show-search');
-		e.preventDefault();
-	});
-	$('.hide-search-form').click(function(e) {
-		$('#search-form').addClass('hide-search').removeClass('show-search');
-		e.preventDefault();
-	})
 	$('#searchsub').click(function() {
 		$('#randomrecipe-app').hide();
 	})
--- assets/js/random-recipe.js
+++ assets/js/random-recipe.js
@@ -5,10 +5,81 @@ Vue.component('randomrecipe', {
 			<div class="" v-for="result in results">
 				<div class="card">
 					<div class="card-title">
-						<h3>{{ result.strMeal }}</h3>
+						<h3>Random Recipe - {{ result.strMeal }}</h3>
 					</div>
 					<div class="card-text">
-						<img :src="result.strMealThumb" />
+						<div id="recipethumb">
+							<img :src="result.strMealThumb" />
+							<h5>Category: {{ result.strCategory }}</h5>
+							<h5 v-if="result.strTags" id="tags">Tags: {{ result.strTags }}</h5>
+						</div>
+						<div id="recipedetails">
+							<h4>Ingredients</h4>
+							<ul>
+								<li v-if="result.strIngredient1">
+									{{result.strIngredient1}} - {{result.strMeasure1}}
+								</li>
+								<li v-if="result.strIngredient2">
+									{{result.strIngredient2}} - {{result.strMeasure2}}
+								</li>
+								<li v-if="result.strIngredient3">
+									{{result.strIngredient3}} - {{result.strMeasure3}}
+								</li>
+								<li v-if="result.strIngredient4">
+									{{result.strIngredient4}} - {{result.strMeasure4}}
+								</li>
+								<li v-if="result.strIngredient5">
+									{{result.strIngredient5}} - {{result.strMeasure5}}
+								</li>
+								<li v-if="result.strIngredient6">
+									{{result.strIngredient6}} - {{result.strMeasure6}}
+								</li>
+								<li v-if="result.strIngredient7">
+									{{result.strIngredient7}} - {{result.strMeasure7}}
+								</li>
+								<li v-if="result.strIngredient8">
+									{{result.strIngredient8}} - {{result.strMeasure8}}
+								</li>
+								<li v-if="result.strIngredient9">
+									{{result.strIngredient9}} - {{result.strMeasure9}}
+								</li>
+								<li v-if="result.strIngredient10">
+									{{result.strIngredient10}} - {{result.strMeasure10}}
+								</li>
+								<li v-if="result.strIngredient11">
+									{{result.strIngredient11}} - {{result.strMeasure11}}
+								</li>
+								<li v-if="result.strIngredient12">
+									{{result.strIngredient12}} - {{result.strMeasure12}}
+								</li>
+								<li v-if="result.strIngredient13">
+									{{result.strIngredient13}} - {{result.strMeasure13}}
+								</li>
+								<li v-if="result.strIngredient14">
+									{{result.strIngredient14}} - {{result.strMeasure14}}
+								</li>
+								<li v-if="result.strIngredient15">
+									{{result.strIngredient15}} - {{result.strMeasure15}}
+								</li>
+								<li v-if="result.strIngredient16">
+									{{result.strIngredient16}} - {{result.strMeasure16}}
+								</li>
+								<li v-if="result.strIngredient17">
+									{{result.strIngredient17}} - {{result.strMeasure17}}
+								</li>
+								<li v-if="result.strIngredient18">
+									{{result.strIngredient18}} - {{result.strMeasure18}}
+								</li>
+								<li v-if="result.strIngredient19">
+									{{result.strIngredient19}} - {{result.strMeasure19}}
+								</li>
+								<li v-if="result.strIngredient20">
+									{{result.strIngredient20}} - {{result.strMeasure20}}
+								</li>
+							</ul>
+							<h4>Instructions</h4>
+							<p>{{ result.strInstructions }}</p>
+						</div>
 					</div>
 				</div>
 			</div>
