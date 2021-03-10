e53ed15598ee777bb7d5af486cf5d59ca552fb4b
Kev <kevleitch@gmail.com>
2021-03-10 21:44:47 +0000

    Added similar recipes

---
+++ assets/js/recipes-like.js
@@ -0,0 +1,56 @@
+Vue.component('recipeslike', {
+	props: ['results'],
+	template: `
+		<section>
+			<h2>Similar Recipes</h2>
+			<div class="" v-for="result in results">
+				<div class="card">
+					<div class="card-text">
+						<div id="recipethumb">
+							<img :src="result.strMealThumb" />
+						</div>
+						<div id="recipedetails">
+							<h4>{{ result.strMeal }}</h4>
+							<a class="btn btn-outline-info" :href="'recipe.html?recipeid=' + result.idMeal" role="button">Get Recipe</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	`
+})
+
+const rl = new Vue({
+	el: '#recipeslike-app',
+	data: {
+		results: [],
+		searchrecipe: ''
+	},
+	methods: {
+		recipeslike:function() {
+			var getUrlParameter = function getUrlParameter(sParam) {
+				var sPageURL = window.location.search.substring(1),
+				sURLVariables = sPageURL.split('&'),
+				sParameterName,
+				i;
+
+				for (i = 0; i < sURLVariables.length; i++) {
+					sParameterName = sURLVariables[i].split('=');
+
+					if (sParameterName[0] === sParam) {
+						return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+					}
+				}		
+			};
+			var cid = getUrlParameter('cat');
+			axios.get("https://www.themealdb.com/api/json/v1/1/filter.php?c="+cid)
+			.then(response => {
+				this.results = response.data.meals;
+				console.log(response);
+			})
+		}
+	},
+	created: function(){
+        this.recipeslike()
+    }
+});
