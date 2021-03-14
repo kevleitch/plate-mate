d58e5f04ddb82495ed1b02fd1ec5178f99ee3089
Kev <kevleitch@gmail.com>
2021-03-14 06:41:54 +0000

    removed bookmarks

--- assets/js/single-recipe.js
+++ assets/js/single-recipe.js
@@ -4,11 +4,6 @@ Vue.component('singlerecipe', {
 		<section>
 			<div class="" v-for="result in results">
 				<div class="card">
-					<div id="bookmark">
-						<a href=""><img src="assets/img/bookmark.png" alt="Bookmark this recipe" /></a>
-						<a href=""><img src="assets/img/fb-icon.png" alt="Share on Facebook" /></a>
-						<a href=""><img src="assets/img/tw-icon.png" alt="Share on Twitter" /></a>
-					</div>
 					<div class="card-title">
 						<h3>{{ result.strMeal }}</h3>
 					</div>
