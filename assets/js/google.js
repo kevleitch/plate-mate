b32c29664af2a33380139f2c8bb4c3402c483b56
Kev <kevleitch@gmail.com>
2021-03-25 16:51:22 +0000

    Added logout link

--- assets/js/google.js
+++ assets/js/google.js
@@ -1,13 +1,14 @@
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
   console.log('ID: ' + profile.getId());
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail());
+  document.getElementById('img-avatar').src = profile.getImageUrl();
+  document.getElementById('img-avatar').setAttribute('title', 'Logged in as: ' + profile.getName());
+  var id_token = googleUser.getAuthResponse().id_token;
 }
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
       console.log('User signed out.');
     });
-  }
+}
