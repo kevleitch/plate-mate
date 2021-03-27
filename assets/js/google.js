600f3cb2ded3d2a365ef475281ca4ebd60a596b5
Kev <kevleitch@gmail.com>
2021-03-27 08:11:53 +0000

    Logged out avatar

--- assets/js/google.js
+++ assets/js/google.js
@@ -3,12 +3,14 @@ function onSignIn(googleUser) {
   console.log('ID: ' + profile.getId());
   console.log('Email: ' + profile.getEmail());
   document.getElementById('img-avatar').src = profile.getImageUrl();
-  document.getElementById('img-avatar').setAttribute('title', 'Logged in as: ' + profile.getName());
+  document.getElementById('img-avatar').setAttribute('title', 'Signed in as: ' + profile.getName());
   var id_token = googleUser.getAuthResponse().id_token;
 }
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
-      console.log('User signed out.');
+		console.log('User signed out.');
+		document.getElementById('img-avatar').src = './assets/img/avatar.png';
+		document.getElementById('img-avatar').setAttribute('title', 'Logged out');
     });
 }
